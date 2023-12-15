7ed281727455964f9d6428bbeaa5851eacf1cae9
GODREACT <137889849+GODREACT@users.noreply.github.com>
2023-12-15 17:47:42 +0900

    12.15

--- src/index.js
+++ src/index.js
@@ -2,7 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import AdminPage from './admin/adminpage';
+// import AdminPage from './admin/adminpage';
+import Ad_suggestion from './admin/ad_suggestion';
 import reportWebVitals from './reportWebVitals';
 import { BrowserRouter } from 'react-router-dom';
 
@@ -10,8 +11,9 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
     <React.StrictMode>
       <BrowserRouter>
-        <App />
+        {/* <App /> */}
         {/* <AdminPage /> */}
+        <Ad_suggestion></Ad_suggestion>
       </BrowserRouter>
     </React.StrictMode>
 );
