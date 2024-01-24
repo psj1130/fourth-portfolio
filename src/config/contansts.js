584d259afb2ee0db962db4aa536703fa4cf4e635
SungJong-Park <zzxxx1@naver.com>
2024-01-24 09:38:39 +0900

    v2

--- src/config/contansts.js
+++ src/config/contansts.js
@@ -1,2 +1,3 @@
 // export const API_URL = "http://localhost:3001";
-export const API_URL = "https://3.137.144.14:3001";
+// export const API_URL = "https://3.137.144.14:3002";
+export const API_URL = "https://tkwk0321.site:3002";
--- src/config/serverurl.js
+++ src/config/serverurl.js
@@ -1,2 +1,2 @@
 // export const API_URL = "http://localhost:3001";
-export const API_URL = "https://3.137.144.14:3001";
+export const API_URL = "https://tkwk0321.site:3002";
